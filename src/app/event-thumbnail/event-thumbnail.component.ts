586ebbd0d68f50aba9909472f8dc97cccd9d5d78
Nikita Stets <nikitas@thomas.co>
2021-10-13 11:58:29 +0300

    ngClass part 1

--- src/app/event-thumbnail/event-thumbnail.component.ts
+++ src/app/event-thumbnail/event-thumbnail.component.ts
@@ -1,20 +1,16 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
   templateUrl: './event-thumbnail.component.html',
   styleUrls: ['./event-thumbnail.component.css']
 })
-export class EventThumbnailComponent implements OnInit {
+export class EventThumbnailComponent {
   @Input() event: any
-  @Output() eventClick = new EventEmitter()
 
-  handleClickMe() {
-    this.eventClick.emit(this.event.name)
-  }
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
 
-  constructor() { }
-
-  ngOnInit(): void {
+    return { green: isEarlyStart, bold: isEarlyStart }
   }
 }
