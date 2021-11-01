6aea8fc68c49d35379409efc103b8a826499800d
Nikita Stets <nikitas@thomas.co>
2021-11-01 18:24:36 +0300

    Add CreateSessionComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,29 @@ import { RouterModule } from '@angular/router';
 
 import { appRoutes } from './routes';
 
-import { EventsAppComponent } from './events-app.component';
-import { EventListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventDetailsComponent } from './event-details/event-details.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { Error404Component } from './errors/404.component';
 
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventRouteActivator } from './event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
+
 import { AuthService } from './user/login/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {
+  EventListResolver,
+  EventService,
+  EventsAppComponent,
+  EventListComponent
+} from './events/index';
+
+import {
+  EventDetailsComponent,
+  EventRouteActivator
+} from './event-details/index';
+
+import { CreateSessionComponent } from './create-session/index';
 
 @NgModule({
   declarations: [
@@ -27,7 +36,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     NavBarComponent,
     EventDetailsComponent,
     CreateEventComponent,
-    Error404Component
+    Error404Component,
+    CreateSessionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,9 @@ import { Routes } from '@angular/router'
 import {
     EventListComponent,
     EventListResolver,
-  } from './events/index'
+} from './events/index'
+
+import { CreateSessionComponent } from './create-session/index';
 
 import { EventDetailsComponent } from "./event-details/event-details.component"
 import { CreateEventComponent } from './create-event/create-event.component';
@@ -11,10 +13,12 @@ import { Error404Component } from './errors/404.component';
 
 import { EventRouteActivator } from './event-details/event-route-activator.service';
 
+
 export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventListComponent, resolve: { events: EventListResolver } },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
+    { path: 'events/session/new', component: CreateSessionComponent },
     { path: '404', component: Error404Component },
     { path: '', redirectTo: '/events', pathMatch: 'full' },
     {
